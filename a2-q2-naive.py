d8da5f3ffc6f5e2679ba400f61bd20f5fc999ec0
Matt Watson <matthewwatson96@gmail.com>
2017-10-14 15:43:42 +1100

    clean

--- a2-q2-naive.py
+++
@@ -1,7 +0,0 @@
-def naive(points):
-    n = len(points)
-    importance = [0] * n
-    for ix,(x,y) in enumerate(points):
-        importance[ix] = len([1 for other_x, other_y in points
-                                if x > other_x and y > other_y])
-    return zip(points, importance)
